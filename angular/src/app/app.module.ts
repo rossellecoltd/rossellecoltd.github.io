8fe0888be7618edf5163817bcf280afc6f8a99dc
Phumin Walaipatchara <phuminwal@gmail.com>
2020-11-14 14:12:19 +0700

    feat: add PGA genuine message

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -13,7 +13,7 @@ import { AboutComponent } from './about/about.component';
 import { ProductsComponent } from './products/products.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './home/home.component';
+// import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +23,7 @@ import { HomeComponent } from './home/home.component';
     ProductsComponent,
     FooterComponent,
     HeaderComponent,
-    HomeComponent,
+    // HomeComponent,
   ],
   imports: [
     BrowserModule,
